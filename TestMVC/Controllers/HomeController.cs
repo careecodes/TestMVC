30d5dc7332e4958a33bb25dc99afc39686488255
Caree Youngman <cyoungman@Carees-MacBook-Air.local>
2017-07-25 14:56:07 -0500

    Initial commit, build a very basic MVC program that outputs first name.

---
+++ TestMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+// Declaring my model in the controller, tying the controller (this) to the model
+using TestMVC.Models;
+
+namespace TestMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+			// Pretending we got this from the Service Layer (db, etc)
+			var foobar = new FoobarView();
+            foobar.FirstName = "Caree";
+
+            // Returning the view, passing the model in, trying the controller (this) to the view
+            return View(foobar);
+        }
+    }
+}
---
+++ TestMVC/Models/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// QUESTION
+// Declaring namespace ---? Why is this necessary? Ask Cole/Taryn
+namespace TestMVC.Models
+                 
+{
+    public class FoobarView
+    {
+        // Declaring variables?
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
