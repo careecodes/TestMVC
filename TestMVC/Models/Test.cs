fd562b13d1f759b403c6416a9783bca8c3ffe265
Caree Youngman <cyoungman@Carees-MacBook-Air.local>
2017-07-25 16:38:44 -0500

    Added question comment

--- TestMVC/Models/Test.cs
+++ TestMVC/Models/Test.cs
@@ -7,9 +7,12 @@ namespace TestMVC.Models
 {
     public class FoobarView
     {
-        // Declaring variables?
+        // Declaring variables? What is this?
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ProfileImagePath { get; set; }
     }
 }
+
+// QUESTION
+// public vs private - "same thing" as immutable vs mutable?
