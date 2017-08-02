afbea52ba60a69724c0cfa1f625ef343d075e907
Caree Youngman <caree@caree.codes>
2017-08-01 21:40:29 -0500

    minor changes index file

--- TestMVC/Repositories/UserRepository.cs
+++ TestMVC/Repositories/UserRepository.cs
@@ -11,41 +11,40 @@ namespace TestMVC.Repositories
         /// </summary>
         /// <returns>The users.</returns>
         public static List<UserModel> GetUsers() {
-            var users = new List<UserModel>();
+            return new List<UserModel>
+            {
+                new UserModel()
+                {
+                    FirstName = "George",
+                    LastName = "Washington",
+                    ProfileImagePath = "http://www.placehold.it/200x200",
+                    presidentialTerm = "1789-1797"
+                },
 
-			var user1 = new UserModel();
-			user1.FirstName = "George";
-			user1.LastName = "Washington";
-			user1.ProfileImagePath = "http://www.placehold.it/200x200";
-            user1.presidentialTerm = "1789-1797";
-			users.Add(user1);
+                new UserModel()
+                {
+                    FirstName = "John",
+                    LastName = "Adams",
+                    ProfileImagePath = "http://www.placehold.it/200x200",
+                    presidentialTerm = "1797-1801"
+                },
 
+                new UserModel()
+                {
+                    FirstName = "Thomas",
+                    LastName = "Jefferson",
+                    ProfileImagePath = "http://www.placehold.it/200x200",
+                    presidentialTerm = "1801-1809"
+                },
 
-			var user2 = new UserModel();
-			user2.FirstName = "John";
-			user2.LastName = "Adams";
-			user2.ProfileImagePath = "http://www.placehold.it/200x200";
-			user2.presidentialTerm = "1797-1801";
-			users.Add(user2);
-
-
-
-			var user3 = new UserModel();
-			user3.FirstName = "Thomas";
-			user3.LastName = "Jefferson";
-			user3.ProfileImagePath = "http://www.placehold.it/200x200";
-			user3.presidentialTerm = "1801-1809";
-			users.Add(user3);
-
-
-			var user4 = new UserModel();
-			user4.FirstName = "James";
-			user4.LastName = "Madison";
-			user4.ProfileImagePath = "http://www.placehold.it/200x200";
-			user4.presidentialTerm = "1809-1817";
-			users.Add(user4);
-
-			return users;
+                new UserModel()
+                {
+                    FirstName = "James",
+                    LastName = "Madison",
+                    ProfileImagePath = "http://www.placehold.it/200x200",
+                    presidentialTerm = "1809-1817"
+                }
+            };
         }
     }
 }
